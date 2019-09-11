b88987a1610dc04a64a5f5cc1a2c4a6c276e42fd
guptarenu240489 <guptarenu24@gmail.com>
2019-09-11 11:16:21 +0530

    injecting service within another service

--- my-first-app/src/app/accounts.service.ts
+++ my-first-app/src/app/accounts.service.ts
@@ -1,4 +1,9 @@
+import { Injectable } from '@angular/core';
+import { LoggingService } from './logging.service';
+
+@Injectable()
 export class AccountService {
+    constructor(private loggingService: LoggingService) { }
     accounts = [
         {
           name: 'Master Account',
@@ -16,9 +21,12 @@ export class AccountService {
 
       onAccountAdded(name: string, status: string) {
         this.accounts.push({name: name, status: status});
+        this.loggingService.logStatusChange(status);
+
       }
 
       onStatusChanged(id: number, newStatus: string) {
         this.accounts[id].status = newStatus;
+        this.loggingService.logStatusChange(newStatus);
       }
 }
--- my-first-app/src/app/new-account/new-account.component.ts
+++ my-first-app/src/app/new-account/new-account.component.ts
@@ -1,19 +1,15 @@
 import { Component } from '@angular/core';
-import { LoggingService } from '../logging.service';
 import { AccountService } from '../accounts.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
 })
 export class NewAccountComponent {
 
-  constructor(private loggingService: LoggingService,
-    private accountService: AccountService) { }
+  constructor(private accountService: AccountService) { }
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountService.onAccountAdded(accountName, accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
   }
 }
