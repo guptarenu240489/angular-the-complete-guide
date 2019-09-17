61c61526f358166c8caf1fa970a64166b02f1c65
guptarenu240489 <guptarenu24@gmail.com>
2019-09-17 11:33:36 +0530

    implemeting basic of reactive form

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,37 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app.routing.module';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
-import { ServersComponent } from './servers/servers.component';
-import { UserComponent } from './users/user/user.component';
-import { EditServerComponent } from './servers/edit-server/edit-server.component';
-import { ServerComponent } from './servers/server/server.component';
-import { ServersService } from './servers/servers.service';
-import { AuthService } from './auth.service';
-import { AuthGuard } from './auth-guard.service';
-import { ServerResolver } from './servers/server/server-resolve.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    UsersComponent,
-    ServersComponent,
-    UserComponent,
-    EditServerComponent,
-    ServerComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    AppRoutingModule
+    FormsModule, // Required for template driven form
+    ReactiveFormsModule // Required for reactive form
   ],
-  providers: [ServersService, AuthService, AuthGuard, ServerResolver],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
